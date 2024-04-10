501a86eb2964e2e6f5e4f97f9980763b833404ee
rustybera <rusty@berachain.com>
2024-04-10 03:05:25 -0400

    Refactor code to use `Verifier` as base contract
    The `ValidatorVerifier` and `WithdrawalsVerifier` contracts have been refactored to use a new base contract called `Verifier`. This helps avoid code duplication as `getParentBlockRoot` function and `BEACON_ROOTS` constant are now defined in the base contract. This change leads to a cleaner, more maintainable, and less error prone codebase.

--- src/WithdrawalsVerifier.sol
+++ src/WithdrawalsVerifier.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.21;
 
 import { SSZ } from "./SSZ.sol";
+import { Verifier } from "./Verifier.sol";
 
-contract WithdrawalsVerifier {
-    address public constant BEACON_ROOTS =
-        0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02;
-
+contract WithdrawalsVerifier is Verifier {
     uint64 internal constant MAX_WITHDRAWALS = 2 << 4;
 
     // Generalized index of the first withdrawal struct root in the withdrawals.
@@ -14,8 +12,6 @@ contract WithdrawalsVerifier {
 
     /// @notice Emitted when a withdrawal is submitted
     event WithdrawalSubmitted(uint64 indexed validatorIndex, uint64 amount);
-
-    error RootNotFound();
 
     constructor(uint256 _gIndex) {
         gIndex = _gIndex;
@@ -49,21 +45,5 @@ contract WithdrawalsVerifier {
         );
 
         emit WithdrawalSubmitted(withdrawal.validatorIndex, withdrawal.amount);
-    }
-
-    function getParentBlockRoot(uint64 ts)
-        internal
-        view
-        returns (bytes32 root)
-    {
-        assembly ("memory-safe") {
-            mstore(0, ts)
-            let success := staticcall(gas(), BEACON_ROOTS, 0, 0x20, 0, 0x20)
-            if iszero(success) {
-                mstore(0, 0x3033b0ff) // RootNotFound()
-                revert(0x1c, 0x04)
-            }
-            root := mload(0)
-        }
     }
 }
